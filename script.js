f90627ed53a3858910e683ac658c14042b9ced6e
Thendelo Romario Mashapha <romariomashinquiries@gmail.com>
2025-07-13 23:24:08 +0200

    code
    created contact and required html and added buttons to home

--- script.js
+++
@@ -1,4 +0,0 @@
-function showForm(formid){
-    document.querySelectorAll(".form-container").forEach(form => form.classList.remove("active"));
-    document.getElementById(formid).classList.add("active");
-}
